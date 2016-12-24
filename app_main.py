b7974fa8775848a380c20289e7c0e9b175266d89
Evan Lohn <evan@Evans-MacBook-Pro.local>
2016-12-24 06:44:16 +0200

    fixed another syntax error (oops)

--- app_main.py
+++ app_main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 class WB2016App(App):
     """The physical App that the user will actually run and interact with.
     Apps generaly start with a titlescreen, and this app should be 
-    renamed once a name is settled upon.""".
+    renamed once a name is settled upon."""
     def build(self):
         """The build method of this App tells the App what to create when 
         it is run. As of now, when the App is run it displays whatever 
