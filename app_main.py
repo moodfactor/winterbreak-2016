af74b493e27e0fd801a04e8145625d43103d98cd
DaMonkeyBoi <darrenwang@berkeley.edu>
2017-01-06 22:59:11 -0800

    just added a builder that forced the py file to get information from the kv file

--- app_main.py
+++ app_main.py
@@ -1,10 +1,12 @@
 from kivy.app import App
+from kivy.lang import Builder
 #from kivy.uix.widget import Widget
 #from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ListProperty
+Builder.load_file("ShapeSmasher.kv")
 
 import random
 
