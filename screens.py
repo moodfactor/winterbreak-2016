b2f1c4d36db85ddd72acf485c2de135a52e94116
Jason Huynh <Jasonhuynh73@gmail.com>
2017-01-06 22:44:21 -0800

    Added blank screen templates
    We can decide on layouts once we design each screen

--- screens.py
+++ screens.py
@@ -13,17 +13,29 @@ class TitleScreen(GridLayout):
     lacks any actualy components."""
     pass
 
+class MainGameScreen():
+    pass
+
+class InstructionScreen():
+    pass
+
+class SettingsScreen():
+    pass
+
+class LeaderboardScreen():
+    pass
+
 class QWERTYScreen(RelativeLayout):
-    """An example of another screen. We might need 4 or 5 screens to 
+    """An example of another screen. We might need 4 or 5 screens to
     fully implement our idea. Creating new screens instead of modifying
     content in a screen you're already working with makes sense when
     there's a drastic change in what the app needs to do, i.e. when the
     user is at the title screen versus when they are actually using the app.
-    
+
     It also might make sense to use something other than gridlaout. If a
     Grid is not how you want to layout your components, search for other
     layouts that fit your preferences/needs!
 
-    https://kivy.org/docs/gettingstarted/layouts.html (Don't be afraid to 
+    https://kivy.org/docs/gettingstarted/layouts.html (Don't be afraid to
     spend some time reading documentation!!!"""
     pass
