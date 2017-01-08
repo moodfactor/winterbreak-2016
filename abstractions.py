b860ebcf16430dbce207162c74ecbc6745ee43f5
Evan Lohn <evan@Evans-MacBook-Pro.local>
2017-01-07 18:11:21 -0800

    Added further game logic concerning updating thegameState in regular time intervals as well as a rudimentary scoring system (all still documentation, no non-trivial code)

--- abstractions.py
+++ abstractions.py
@@ -4,7 +4,9 @@ displayed in the main game screen."""
 from kivy.uix.button import Button
 
 class Shape(Button):
-    def __init__(self, shape_type, side_length, color_str, timer=None, clear_method='tap', health=1):
+    def __init__(self, shape_type, side_length, color_str, game_state=None, timer=None, clear_method='tap', health=1):
+        
+
         self.shape_type = shape_type
         """ shape_type will be a string describing the type of shape_type
         i.e. triangle, hexagon, or square that will be tesselated to form the grid"""
@@ -21,6 +23,12 @@ class Shape(Button):
         values are RGBA lists as used by kivy.
         Additionally, there will be a list of color strings that can be randomly pulled
         from in order to randomize colors."""
+
+        self.game_state = game_state
+        """game_state is a reference to the GameState instance in which this Shape is being
+        placed. It is initially defaulted to None, and is initialized either upon the construction of 
+        the GameState instance that this Shape is added to or upon this Shape's construction if
+        the Shape is being added to a previously existing GameState instance."""
 
         self.timer = timer
         """ Describes how much time is left to clear the shape. Default is None for
@@ -86,8 +94,8 @@ class Row():
     def remove_shape(self, index):
         """This method removes the shape at the given list index
         from this Row's list of shapes by setting the list value 
-        at that index to be None."""
-
+        at that index to be None. it also uses the Shape's stored
+        GameState to increase the score of that gameState by 10 points."""
 
 
 class GameState():
@@ -104,12 +112,24 @@ class GameState():
     as a Widget of some sort in which the contents of this game will 
     actually be displayed."""
 
+    mode_dispatch = {'timed': (update_timed_mode,1) , 'health': (update_health_mode, 0.25)}
+    #dispatch dictionary for different game modes and update times
+
+
     def __init__(self, game_mode, row_list, game_screen):
         """The constructor for a GameState requires both the type of
         Game being played, the list of rows that describes 
         the current state of the Game, and the widget on which the game
         will be displayed. The constructor maeks sure these parameters 
-        are formatted correctly and stores them as instance attributes."""
+        are formatted correctly and stores them as instance attributes.
+
+        This constructor calculates and sets an integer instance variable 
+        representing the number of empty spaces (spaces without a shape) 
+        in the game at its beginning, i.e. the total number of places in the
+        grid. It also sets an instance variable score, which will start at 0.
+        This constructor must also iterate through each shape in each Row 
+        (if there are any non-None Shapes to start with) to make sure those
+        Shapes have a correctly initialized GameState attribute."""
         pass
 
     def draw(self):
@@ -125,7 +145,56 @@ class GameState():
         some event such as a swipe or shake is detected. This method
         will iterate through each row and each non-None Shape, updating 
         the data (most likely health) of the shape if necessary based 
-        on the event. This method does not modify the game_screen until
+        on the event. If the health of a shape reaches 0, that Shape
+        is removed and the gameState's instance variable describing number
+        of open spaces is decremented to reflect this.
+
+        This method does not modify the game_screen until
         the last line, which will be a call to the draw() method."""
         pass
      
+    def update(self):
+        """This method is a general purpose update method for the gameState
+        that is mean to be called on regular intervals of time. The 
+        method simply makes a call to the correct update method for this GameState's
+        game mode as determined by the game_mode string (handled with a dispatch dictionary)."""
+        pass
+
+    def update_timed_mode(self):
+        """This method is used specifically to update the game in timed mode.
+        This method will be called once every second and will reduce each Shape's
+        simple integer clock by 1. If any of these clocks goes to 0, this method 
+        stops the game by calling the game_over method. It also adds Shapes to the
+        abstracted grid of Shapes.
+
+        Here's the way adding Shapes will work:
+        As you traverse the list of Rows which each contain lists of shapes, every
+        empty space (i.e. every time a list has a None entry) will have a 0.15 chance
+        to get a new Shape put in. However, a count of the number of shapes added will
+        be updated as you traverse all the spots in the grid as well as a list of 
+        coordinate pairs missing shapes. If at any point during the traversal the 
+        number of shapes added exceeds 2/3 of the number of empty spaces remaining in
+        the grid, no more shapes will be added. Conversely, if your program has added 
+        less than 5 new shapes after it has finished traversing, select random coordinate
+        pairs from the temporary list you have been maintaining to put new Shapes into
+        until 5 new Shapes have been placed or the board is full. 
+
+        This will ensure wild chaos and hilarity as differing numbers of shapes are 
+        added every second in random places."""
+        pass
+
+    def update_health_mode(self):
+        """This method is used to update the game in health mode, a mode where shapes 
+        with variable health values are added quickly to the screen and the player loses
+        if the entire grid fills up with shapes. (Alex pls finsh am tired no quiero trabajar)"""
+        pass
+
+
+
+    def game_over(self):
+        """This method should be called when a condition is reached such that the game
+        is over. this should stop the game's updating loop somehow and use the screen 
+        to exit and go to the leaderboard screen. The action of going to the leaderboard 
+        should also carry with it the score stored by this gameState so it can be placed 
+        onto the leaderboard."""
+        pass
